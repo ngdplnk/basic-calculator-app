d11ca9272fa65e90a6179e6882a4eff10eebe443
ngdplnk <nikkotilion@gmail.com>
2024-05-01 16:18:10 -0400

    private build 6 - changed to dark theme and fixed screen elements sizes

--- android-app/.buildozer/android/app/main.py
+++ android-app/.buildozer/android/app/main.py
@@ -1,117 +1,141 @@
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
 from kivymd.app import MDApp
 from kivymd.uix.button import MDFlatButton
+from kivymd.uix.label import MDLabel
 
-# KV Language string for the UI layout
 kv_string = """
-BoxLayout:
-    orientation: 'vertical'
+AnchorLayout:
+    adaptive_height: True
+    adaptive_width: True
     anchor_x: 'center'
     anchor_y: 'center'
-    
-    MDTextField:
-        id: text_field
-        hint_text: ''
-        font_size: '40sp'
-        readonly: True
-        padding: [10, 20]
-        
-    GridLayout:
-        cols: 4
-        padding: [10, 20]
-        
-        MDFlatButton:
-            text: 'AC'
-            on_press: app.clear_text_field()
-        
-        MDFlatButton:
-            text: '('
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: ')'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '%'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '7'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '8'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '9'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '/'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '4'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '5'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '6'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '*'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '1'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '2'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '3'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '-'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '.'
-            on_press: app.on_button_press(self.text)
-        
-        MDFlatButton:
-            text: '0'
-            on_press: app.on_button_press(self.text)
+    canvas.before:
+        Color:
+            rgba: 0.267, 0.267, 0.267, 1  # Background color (#444444)
+        Rectangle:
+            pos: self.pos
+            size: self.size
 
-        MDFlatButton:
-            text: '<-'
-            on_press: app.backspace()
+    BoxLayout:
+        orientation: 'vertical'
+        size_hint: 0.5, 0.5  # Set the size of the widget to 50 percent of its parent's size
+        pos_hint: {'center_x': 0.5, 'center_y': 0.5}  # Set the position of the widget to the center of its parent
+        adaptive_height: True
+        adaptive_width: True
 
-        MDFlatButton:
-            text: '+'
-            on_press: app.on_button_press(self.text)
+        MDTextField:
+            id: text_field
+            hint_text: 'Your expression goes here'
+            font_size: '40sp'
+            readonly: True
+            padding: [10, 20]
+            color: 1, 1, 1, 1  # Text color (white)
         
-        MDFlatButton:
-            text: '='
-            on_press: app.calculate()
+        GridLayout:
+            cols: 4
+            size_hint: 1, 1  # Set the size of the widget to 100 percent of its parent's size
+            pos_hint: {'center_x': 0.5, 'center_y': 0.5}  # Set the position of the widget to the center of its parent
+            padding: [10, 20]
+            adaptive_height: True
+            adaptive_width: True
+            anchor_x: 'center'
+            anchor_y: 'center'
+
+            MDFlatButton:
+                text: 'AC'
+                on_press: app.clear_text_field()
+
+            MDFlatButton:
+                text: '('
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: ')'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '%'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '7'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '8'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '9'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '/'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '4'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '5'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '6'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '*'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '1'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '2'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '3'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '-'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '.'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '0'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '<-'
+                on_press: app.backspace()
+
+            MDFlatButton:
+                text: '+'
+                on_press: app.on_button_press(self.text)
+
+            MDFlatButton:
+                text: '='
+                on_press: app.calculate()
 """
 
 class CalculatorApp(MDApp):
     def build(self):
-        self.title = 'Basic Calculator PRIVATE BUILD 4'
+        self.title = 'Basic Calculator PRIVATE BUILD 6'
         self.icon = 'icon.png'
         # ICON ATTRIBUTION
         # <a href="https://www.flaticon.es/iconos-gratis/calculadora" title="calculadora iconos">Calculadora iconos creados por Freepik - Flaticon</a>
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Green"
         self.expression = ''
         return Builder.load_string(kv_string)
 
--- android-app/buildozer.spec
+++ android-app/buildozer.spec
@@ -1,7 +1,7 @@
 [app]
 
 # (str) Title of your application
-title = Basic Calculator PRIVATE BUILD 5
+title = Basic Calculator PRIVATE BUILD 6
 
 # (str) Package name
 package.name = basiccalc
@@ -29,7 +29,7 @@ source.include_exts = py,png,jpg,kv,atlas
 #source.exclude_patterns = license,images/*/*.jpg
 
 # (str) Application versioning (method 1)
-version = 0.5
+version = 0.6
 
 # (str) Application versioning (method 2)
 # version.regex = __version__ = ['"](.*)['"]
