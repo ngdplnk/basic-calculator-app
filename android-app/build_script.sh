0b287fbbdc9ab0c77550a1e1fc423100abd63495
Nicolás Valdés <nikkotilion@gmail.com>
2024-05-04 12:44:36 -0400

    added confirmations to buildozer process

--- android-app/build_script.sh
+++ android-app/build_script.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-# Install buildozer
-pip install buildozer
-
-# Run buildozer command
-buildozer android debug
+# Run buildozer logic
+expect -c '
+spawn buildozer android debug
+expect {
+    "Accept? (y/N):" {
+        send "y\r"
+        exp_continue
+    }
+    "Accept? (y/N):" {
+        send "y\r"
+        exp_continue
+    }
+    "Accept? (y/N):" {
+        send "y\r"
+        exp_continue
+    }
+    "Accept? (y/N):" {
+        send "y\r"
+        exp_continue
+    }
+    "Accept? (y/N):" {
+        send "y\r"
+        exp_continue
+    }
+    # Add more patterns and responses as needed
+    eof
+}
+'
