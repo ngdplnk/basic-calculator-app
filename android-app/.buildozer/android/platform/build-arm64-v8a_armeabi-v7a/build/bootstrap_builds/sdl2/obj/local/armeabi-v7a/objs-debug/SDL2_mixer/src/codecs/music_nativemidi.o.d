8744507e212321910ce7f621590972808f5cbe9b
Nicolás Valdés <nikkotilion@gmail.com>
2024-05-05 14:42:52 -0400

    Delete android-app/.buildozer directory

--- android-app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/SDL2_mixer/src/codecs/music_nativemidi.o.d
+++
@@ -1,2 +0,0 @@
-/home/nicolas/Documents/basic-calculator-app/android-app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/SDL2_mixer/src/codecs/music_nativemidi.o: \
-  /home/nicolas/Documents/basic-calculator-app/android-app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_mixer/src/codecs/music_nativemidi.c
--- android-app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/basiccalc/obj/local/arm64-v8a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/nicolas/Documents/basic-calculator-app/android-app/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/nicolas/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.c \
-  /home/nicolas/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.h
-
-/home/nicolas/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.h:
